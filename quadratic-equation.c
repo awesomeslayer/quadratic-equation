91f41c7f1444ecc2aa114fb6021d312e198db7f0
fatalick01 <89474980+fatalick01@users.noreply.github.com>
2021-08-27 12:54:24 +0300

    Update quadratic-equation.c

--- quadratic-equation.c
+++ quadratic-equation.c
@@ -1,37 +1,79 @@
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
 
-const int INF_ROOTS = -1;
-const double eps = 1E-5;
-const int co
+const double EPS = 1E-5;
 
-double equal(double a, double b)
+enum SolveStatus
 {
-    if (fabs(a - b) <= eps)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+INF_ROOTS = -1,
+Correct_num = 3,
+YES = 1,
+NO = 0
+};
+
+void swap(double* a, double* b)
+{
+    double c = *a;
+
+    *a = *b;
+    *b = c;
 }
 
-int SolveLine(double b, double c, double *x1)
+void SortRoots(double *TRoot1, double *TRoot2, double *x1, double *x2)
 {
-    if (b == 0 && c == 0)
+    if(*TRoot1 < *TRoot2)
+    {
+        swap(TRoot1, TRoot2);
+    }
+
+    if(*x1 < *x2)
+    {
+        swap(x1, x2);
+    }
+
+    assert(*x1 >= *x2);
+    assert(*TRoot1 >= *TRoot2);
+
+}
+
+bool equal(double val1, double val2)
+{
+    assert (isfinite (val1));
+    assert (isfinite (val2));
+
+    return (fabs(val1-val2) < EPS)? YES : NO;
+}
+
+int SolveLine(double b, double c, double *x1, double *x2)
+{
+    assert(isfinite (b));
+    assert(isfinite (c));
+    assert(isfinite (*x1));
+
+    if (equal(b, 0) && equal(c, 0))
     {
         return -1;
     }
     else
     {
-        *x1 = -(c / b);
+        *x1 = *x2 = -(c / b);
         return 1;
     }
 }
 
 int SolveQuad(double a, double b, double c, double *x1, double *x2)
 {
+    assert(isfinite (a));
+    assert(isfinite (b));
+    assert(isfinite (c));
+    assert(isfinite (*x1));
+    assert(isfinite (*x2));
+
+    assert(*x1 == *x2);
+    assert(*x1 == 0);
+    assert(*x2 == 0);
+
     double D = b * b - 4 * a * c;
 
     if (D > 0)
@@ -40,9 +82,9 @@ int SolveQuad(double a, double b, double c, double *x1, double *x2)
         *x2 = (-b - sqrt(D)) / (2 * a);
         return 2;
     }
-    else if (equal(D, 0) == 1)
+    else if (equal(D, 0))
     {
-        *x1 = -b / (2 * a);
+        *x1 = *x2 = -b / (2 * a);
         return 1;
     }
     else
@@ -53,14 +95,56 @@ int SolveQuad(double a, double b, double c, double *x1, double *x2)
 
 int Solve(double a, double b, double c, double *x1, double *x2)
 {
-    if (a == 0)
+    assert (isfinite (a));
+    assert (isfinite (b));
+    assert (isfinite (c));
+
+    if (equal(a, 0))
     {
-        return SolveLine(b, c, x1);
+        return SolveLine(b, c, x1, x2);
     }
     else
     {
         return SolveQuad(a, b, c, x1, x2);
     }
+}
+
+void SoloTestRoot(double a, double b, double c, int TrueNumRoots, double TrueRoot1, double TrueRoot2, int nTest, int* NumFail)
+{
+    double x1 = 0;
+    double x2 = 0;
+
+    SortRoots(&TrueRoot1, &TrueRoot2, &x1, &x2);
+
+    int NumRoots = Solve (a, b, c, &x1, &x2);
+
+    if (NumRoots != TrueNumRoots || !equal(x1, TrueRoot1) && !equal(x2, TrueRoot2))
+    {
+        printf("Mistake in Test%d with coefficients a = %lf, b = %lf, c = %lf\n"
+               "And roots: x1 = %lf (instead of %lf) , x2 = %lf (instead of %lf)\n",
+                nTest, a, b, c, x1, TrueRoot1, x2, TrueRoot2);
+
+        *NumFail = *NumFail + 1;
+    }
+    else
+    {
+        printf("OK in Test%d\n", nTest);
+    }
+}
+
+int TestSolve()
+{
+    int NumFail = 0;
+
+    SoloTestRoot(0, 0, 0, INF_ROOTS, NO, NO, 1, &NumFail);
+    SoloTestRoot(1, 2, 3, 0, NO, NO, 2, &NumFail);
+    SoloTestRoot(1, 2, -3, 2, 1, -3, 3, &NumFail);
+    SoloTestRoot(0, 2, 1, 1, -0.5, -0.5, 4, &NumFail);
+    SoloTestRoot(1, 2, 1, 1, -1, -1, 5, &NumFail);
+
+    assert(NumFail == 0);
+
+    return NumFail;
 }
 
 
@@ -69,42 +153,48 @@ int main()
     double a_coefficient = 0;
     double b_coefficient = 0;
     double c_coefficient = 0;
-    double discriminant = 0;
     double root1 = 0;
     double root2 = 0;
     int NumSolutions = 0;
     int ans = 0;
+    int NumFailedTests = 0;
 
-    while (ans != correct_num)
+    while (ans != Correct_num)
     {
         printf("Please, enter the coefficients of the quadratic equation\n");
         ans = scanf("%lf %lf %lf", &a_coefficient, &b_coefficient, &c_coefficient);
         fflush(stdin);
     }
 
+
     NumSolutions = Solve(a_coefficient, b_coefficient, c_coefficient, &root1, &root2);
+
+    assert(NumSolutions <= Correct_num);
 
     switch (NumSolutions)
     {
     case 0:
-        printf("This equation has no solution");
+        printf("This equation has no solution\n");
         break;
 
     case 1:
-        printf("This equation has 1 solution: x1 = %lf", root1);
+        printf("This equation has 1 solution: x1 = %lf\n", root1);
         break;
 
     case 2:
-        printf("This equation has 2 solutions: x1 = %lf, x2= %lf", root1, root2);
+        printf("This equation has 2 solutions: x1 = %lf, x2= %lf\n", root1, root2);
         break;
 
     case INF_ROOTS:
-        printf("This equation has an infinite number of solutions");
+        printf("This equation has an infinite number of solutions\n");
         break;
 
     default:
         printf("main(): ERROR: number of roots = %d\n", NumSolutions);
     }
 
+    NumFailedTests = TestSolve();
+
+    printf("Failed Tests: %d \n", NumFailedTests);
     return 0;
 }
